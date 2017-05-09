a68d65475e65d5dab21da71b67588074a8c94999
ratanaksambath <ratanaksambath@gmail.com>
2017-05-09 14:33:53 -0700

    change to todo LIst

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -1,26 +1,62 @@
-import os
 from flask import Flask
-from flask_restful import Resource, Api
-from flask_pymongo import PyMongo
-from flask import make_response
-from bson.json_util import dumps
-
-MONGO_URL = os.environ.get('MONGO_URL')
-if not MONGO_URL:
-    MONGO_URL = "mongodb://localhost:27017/rest";
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
-
-app.config['MONGO_URI'] = MONGO_URL
-mongo = PyMongo(app)
-
-def output_json(obj, code, headers=None):
-    resp = make_response(dumps(obj), code)
-    resp.headers.extend(headers or {})
-    return resp
-
-DEFAULT_REPRESENTATIONS = {'application/json': output_json}
 api = Api(app)
-api.representations = DEFAULT_REPRESENTATIONS
 
-import flask_rest_service.resources
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
